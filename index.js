10d452c232bed0670c3f96a987e59d4edf3f7e2c
wd-mahin <wdmahin.mhm@gmail.com>
2022-12-22 21:56:23 +0600

    basic server created

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Server is running");
+})
+
+app.listen(port, (req, res) => {
+    console.log(`Server is listening on port ${port}`);
+});
